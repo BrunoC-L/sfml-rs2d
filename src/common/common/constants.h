5dbc00e7bc0189ccf76306dc8395e694bb244e71
AntoineMartinPoly <antoine24888@hotmail.fr>
2022-02-26 18:00:47 -0500

    fixed map not showing after logging. Added web-client constant generation to python script

--- src/common/common/constants.h
+++ src/common/common/constants.h
@@ -2,10 +2,10 @@
 This file was automatically generated, please do not modify manually, see "python scripts/constants.py"
 */
 #define MAX_PLAYERS_ONLINE 2000
-#define MSPT 60
+#define MSPT 600
 #define CHUNK_RADIUS 2
-#define PIXELS_PER_TILE 4
+#define PIXELS_PER_TILE 32
 #define PIXELS_PER_TILE_ON_MAP 4
 #define TILES_PER_CHUNK 64
-#define TEXTURES_FOLDER std::string("textures-4")
+#define TEXTURES_FOLDER std::string("textures-32")
 #define CHUNKS_FOLDER std::string("chunks-64")
