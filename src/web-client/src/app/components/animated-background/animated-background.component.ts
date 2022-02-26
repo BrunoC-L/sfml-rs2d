f13851dc0cc266707470a75ebd93d72398f33794
Simon Robatto <simon.robatto-simard@polymtl.ca>
2022-02-26 18:22:51 -0500

    Added homescreen with login & signup

---
+++ src/web-client/src/app/components/animated-background/animated-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-background',
+  templateUrl: './animated-background.component.html',
+  styleUrls: ['./animated-background.component.scss']
+})
+export class AnimatedBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
